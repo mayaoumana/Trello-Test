b93b80d001c9c728698e7a65a3d309bb15c0b902
mayaoumana <oumana.raissi@gmail.com>
2023-06-28 13:41:21 +0200

    add timout

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
- defaultCommandTimeout: 10000,
+ defaultCommandTimeout: 60000,
 
   reporter: "cypress-mochawesome-reporter",
   e2e: {
